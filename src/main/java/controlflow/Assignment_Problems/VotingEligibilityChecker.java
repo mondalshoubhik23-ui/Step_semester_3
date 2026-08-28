c8aa36fefdc0d9e4fdb5472ba1af6737a463253d
Shoubhikmondal892-Student <shoubhikmondal892@gmail.com>
2026-08-28 23:41:55 +0530

    Added session 1 assignment problems

---
+++ src/main/java/controlflow/Assignment_Problems/VotingEligibilityChecker.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class VotingEligibilityChecker {
+
+    static void checkVotingEligibility(int age) {
+        if (age >= 18)
+            System.out.println("Eligible to vote");
+        else
+            System.out.println("Not eligible to vote");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+
+        checkVotingEligibility(age);
+    }
+}
