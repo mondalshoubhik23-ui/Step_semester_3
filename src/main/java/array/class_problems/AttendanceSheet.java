9d1e0782e733a00078f6b7921aba110e2cbad12f
Shoubhikmondal892-Student <shoubhikmondal892@gmail.com>
2026-09-20 14:57:46 +0530

    Add attendance sheet

---
+++ src/main/java/array/class_problems/AttendanceSheet.java
@@ -0,0 +1,60 @@
+package array.class_problems;
+
+public class AttendanceSheet {
+
+    private String[] students;
+    private int presentCount;
+
+    public AttendanceSheet(int maxStudents) {
+        students = new String[maxStudents];
+        presentCount = 0;
+    }
+
+    public void markPresent(String name) {
+
+        if (isPresent(name)) {
+            return;
+        }
+
+        if (presentCount < students.length) {
+            students[presentCount] = name;
+            presentCount++;
+        }
+    }
+
+    public int getPresentCount() {
+        return presentCount;
+    }
+
+    public boolean isPresent(String name) {
+
+        for (int i = 0; i < presentCount; i++) {
+            if (students[i].equals(name)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        AttendanceSheet sheet = new AttendanceSheet(30);
+
+        sheet.markPresent("Ana");
+        sheet.markPresent("Ben");
+        sheet.markPresent("Ana");
+
+        System.out.println(
+            "Present count: " + sheet.getPresentCount()
+        );
+
+        System.out.println(
+            "Is Ben present: " + sheet.isPresent("Ben")
+        );
+
+        System.out.println(
+            "Is Chen present: " + sheet.isPresent("Chen")
+        );
+    }
+}
