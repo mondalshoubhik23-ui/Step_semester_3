b003030738f836a597f3de9651ae4f20ab1220d2
Shoubhikmondal892-Student <shoubhikmondal892@gmail.com>
2026-09-20 14:45:32 +0530

    Add shopping cart encapsulation

---
+++ src/main/java/array/assignment_problems/Cart.java
@@ -0,0 +1,49 @@
+package array.assignment_problems;
+
+public class Cart {
+
+    private double[] prices;
+    private int itemCount;
+    private final String cartId;
+
+    public Cart(String cartId, int maxItems) {
+        this.cartId = cartId;
+        this.prices = new double[maxItems];
+        this.itemCount = 0;
+    }
+
+    public void addItem(double price) {
+        if (itemCount < prices.length) {
+            prices[itemCount] = price;
+            itemCount++;
+        } else {
+            System.out.println("Cart is full");
+        }
+    }
+
+    public double getTotal() {
+        double total = 0;
+
+        for (int i = 0; i < itemCount; i++) {
+            total += prices[i];
+        }
+
+        return total;
+    }
+
+    public int getItemCount() {
+        return itemCount;
+    }
+
+    public static void main(String[] args) {
+
+        Cart cart = new Cart("CART-5", 20);
+
+        cart.addItem(250);
+        cart.addItem(99);
+        cart.addItem(151);
+
+        System.out.println("Total: " + cart.getTotal());
+        System.out.println("Item count: " + cart.getItemCount());
+    }
+}
