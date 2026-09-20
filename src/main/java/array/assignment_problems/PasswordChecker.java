13e0fd151c3df8639d14b45d38ee399f574f9e3f
Shoubhikmondal892-Student <shoubhikmondal892@gmail.com>
2026-09-20 14:36:14 +0530

    Add password checker

---
+++ src/main/java/array/assignment_problems/PasswordChecker.java
@@ -0,0 +1,40 @@
+package array.assignment_problems;
+
+public class PasswordChecker {
+
+    private final String password;
+
+    public PasswordChecker(String password) {
+        this.password = password;
+    }
+
+    public String getStrength() {
+
+        int length = password.length();
+
+        if (length < 6) {
+            return "Weak";
+        } else if (length <= 9) {
+            return "Medium";
+        } else {
+            return "Strong";
+        }
+    }
+
+    public static void main(String[] args) {
+
+        PasswordChecker pc =
+            new PasswordChecker("abcd");
+
+        PasswordChecker pc2 =
+            new PasswordChecker("abcdefghij");
+
+        System.out.println(
+            "Password strength: " + pc.getStrength()
+        );
+
+        System.out.println(
+            "Password strength: " + pc2.getStrength()
+        );
+    }
+}
