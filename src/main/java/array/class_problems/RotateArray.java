e4a3a485754577a12b7c3947f3da2068c1fe3d50
Shoubhikmondal892-Student <shoubhikmondal892@gmail.com>
2026-09-20 11:56:22 +0530

    Add rotate array problem

---
+++ src/main/java/array/class_problems/RotateArray.java
@@ -0,0 +1,29 @@
+package array.class_problems;
+
+import java.util.Arrays;
+
+public class RotateArray {
+
+    public static int[] rotateArray(int[] nums, int k) {
+
+        k = k % nums.length;
+
+        int[] result = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            result[(i + k) % nums.length] = nums[i];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+
+        int[] result = rotateArray(nums, k);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
