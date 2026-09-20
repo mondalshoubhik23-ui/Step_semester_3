cced0f3e085a30ccc22649c67ae4e036796700de
Shoubhikmondal892-Student <shoubhikmondal892@gmail.com>
2026-09-20 13:57:28 +0530

    Add employee profile creation

---
+++ src/main/java/array/class_problems/EmployeeProfile.java
@@ -0,0 +1,41 @@
+package array.class_problems;
+
+public class EmployeeProfile {
+
+    String empId;
+    String empName;
+    double salary;
+    boolean isIntern;
+
+    public EmployeeProfile(String empId, String empName, double salary) {
+        this.empId = empId;
+        this.empName = empName;
+        this.salary = salary;
+        this.isIntern = false;
+    }
+
+    public EmployeeProfile(String empId, String empName) {
+        this(empId, empName, 0);
+        this.isIntern = true;
+    }
+
+    public void printProfile() {
+        System.out.println(
+            empId + " | " + empName
+            + " | Rs " + salary
+            + " | Intern: " + isIntern
+        );
+    }
+
+    public static void main(String[] args) {
+
+        EmployeeProfile permanent =
+            new EmployeeProfile("E-101", "Divya", 65000);
+
+        EmployeeProfile intern =
+            new EmployeeProfile("E-102", "Arjun");
+
+        permanent.printProfile();
+        intern.printProfile();
+    }
+}
