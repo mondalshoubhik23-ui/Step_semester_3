594da460401c8668cfa422c0e524188502cc0f02
Shoubhikmondal892-Student <shoubhikmondal892@gmail.com>
2026-09-20 12:00:23 +0530

    Add 3Sum solution

---
+++ src/main/java/array/assignment_problems/ThreeSum.java
@@ -0,0 +1,108 @@
+package array.assignment_problems;
+
+import java.util.Arrays;
+
+public class ThreeSum {
+
+    public static int[][] threeSum(int[] nums) {
+
+        Arrays.sort(nums);
+
+        int count = 0;
+
+        // First pass: count unique triplets
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    count++;
+
+                    int leftValue = nums[left];
+                    int rightValue = nums[right];
+
+                    while (left < right && nums[left] == leftValue) {
+                        left++;
+                    }
+
+                    while (left < right && nums[right] == rightValue) {
+                        right--;
+                    }
+
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        // Create result array
+        int[][] result = new int[count][3];
+
+        int index = 0;
+
+        // Second pass: store unique triplets
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+
+                    result[index++] = new int[]{
+                        nums[i],
+                        nums[left],
+                        nums[right]
+                    };
+
+                    int leftValue = nums[left];
+                    int rightValue = nums[right];
+
+                    while (left < right && nums[left] == leftValue) {
+                        left++;
+                    }
+
+                    while (left < right && nums[right] == rightValue) {
+                        right--;
+                    }
+
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+
+        int[][] result = threeSum(nums);
+
+        for (int[] triplet : result) {
+            System.out.println(Arrays.toString(triplet));
+        }
+    }
+}
